dd6986da78ff6356c51ec3b1760507d3e974b44d
Jean-Luc <an.obscurity@gmail.com>
2016-12-26 11:23:52 +1100

    Added install file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='opticalmaterialspy',
+      version='0.1',
+      description='Python library with optical material properties.',
+      url='https://github.com/jtambasco/opticalmaterialspy',
+      author='Jean-Luc Tambasco',
+      author_email='an.obscurity@gmail.com',
+      license='MIT',
+      packages=['opticalmaterialspy'],
+      zip_safe=False)
